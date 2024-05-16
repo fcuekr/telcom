366610e4ef956103b4238ebe8ff4be33679d7f12
dt_4337977465 <1014155959@qq.com>
2024-05-16 10:35:58 +0800

    链表反转和双链表反转

---
+++ src/main/java/test/maths/ReversalDoubleNode.java
@@ -0,0 +1,68 @@
+package test.maths;
+
+import lombok.AllArgsConstructor;
+
+public class ReversalDoubleNode {
+
+    public static void main(String[] args) {
+        Node node3 = new Node(3, null, null);
+        Node node2 = new Node(2, node3, null);
+        node3.last=node2;
+        Node head = new Node(1, node2, null);
+        node2.last=head;
+
+        Node node4 = new Node(4, null, node3);
+        Node node5 = new Node(5, null, node4);
+        node3.next=node4;
+        node4.next=node5;
+        print(head);
+
+        Node node = reversalDoubleNode2(head);
+        print(node);
+    }
+
+    //----------------算法---------------------
+    private static Node reversalDoubleNode(Node head){
+        Node pre = null;
+        Node next;
+        while (head!=null){
+            next = head.next;
+            pre = head.last;
+            head.next=pre;
+            head.last=next;
+            head = next;
+        }
+        pre=pre.last;
+        return pre;
+    }
+    //----------------算法---------------------
+
+    private static Node reversalDoubleNode2(Node head){
+        Node pre = null;
+        Node next = null;
+        while (head!=null){
+            next = head.next;
+            head.last = next;
+            head.next = pre;
+            pre = head;
+            head=next;
+        }
+        return pre;
+    }
+
+    @AllArgsConstructor
+    static class Node{
+        Integer value;
+        Node next;
+        Node last;
+    }
+
+    private static void print(Node head){
+        if (head==null){
+            return;
+        }
+        System.out.println(head.value);
+        head=head.next;
+        print(head);
+    }
+}
---
+++ src/main/java/test/maths/ReversalNode.java
@@ -0,0 +1,124 @@
+package test.maths;
+
+@SuppressWarnings("all")
+public class ReversalNode {
+
+    public static void main(String[] args) {
+        Node node = new Node(
+                new Node(
+                        new Node(
+                                new Node(
+                                        new Node(null, 5
+                                        ), 4
+                                ), 3
+                        ), 2
+                ), 1
+        );
+        print(node);
+        Node reversalNode = reversalNode8(node);
+        print(reversalNode);
+    }
+
+    private static Node reversalNode(Node node){
+        if (node==null||node.next==null){
+            return node;
+        }
+        Node resultNode = reversalNode(node.next);
+        node.next.next=node;
+        node.next=null;
+        return resultNode;
+    }
+
+    private static Node reversalNode2(Node node){
+        Node resultNode = null;
+        while (node!=null){
+            Node nextNode = node.next;
+            node.next = resultNode;
+            resultNode = node;
+            node = nextNode;
+        }
+        return resultNode;
+    }
+
+    private static Node reversalNode3(Node node){
+        if (node ==null||node.next==null){
+            return node;
+        }
+        Node resultNode = reversalNode3(node.next);
+        node.next.next=node;
+        node.next=null;
+        return resultNode;
+    }
+
+    private static Node reversalNode4(Node node){
+        Node resultNode =null;
+        while (node!=null){
+            Node nextNode =node.next;
+            node.next=resultNode;
+            resultNode = node;
+            node = nextNode;
+        }
+        return resultNode;
+    }
+
+    private static Node reversalNode5(Node node){
+        if (node==null||node.next==null){
+            return node;
+        }
+        Node resultNode = reversalNode5(node.next);
+        node.next.next = node;
+        node.next = null;
+        return resultNode;
+    }
+
+    private static Node reversalNode6(Node node){
+        Node resultNode = null;
+        Node next = null;
+        while (node!=null){
+            next = node.next;
+            node.next = resultNode;
+            resultNode = node;
+            node = next;
+        }
+        return resultNode;
+    }
+
+    private static Node reversalNode7(Node node){
+        if (node==null||node.next==null){
+            return node;
+        }
+        Node resultNode = null;
+        resultNode = reversalNode7(node.next);
+        node.next.next = node;
+        node.next = null;
+        return resultNode;
+    }
+
+    private static Node reversalNode8(Node head){
+        Node resultNode = null;
+        while (head!=null){
+            Node nextNode = head.next;
+            head.next = resultNode;
+            resultNode = head;
+            head = nextNode;
+        }
+        return resultNode;
+    }
+
+     static class Node{
+        Node next;
+        Integer value;
+
+        Node(Node next,Integer value){
+            this.next=next;
+            this.value=value;
+        }
+    }
+
+    private static void print(Node node){
+        if (node!=null){
+            System.out.println(node.value);
+            print(node.next);
+        }
+    }
+}
