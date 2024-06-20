127972d98fb1206a47ce72e56fc2d8629b76abe5
dt_4337977465 <1014155959@qq.com>
2024-06-20 17:39:53 +0800

    不用+-*/,实现加减乘除运算

--- src/main/java/test/maths/I_ComputerMath.java
+++ src/main/java/test/maths/I_ComputerMath.java
@@ -2,14 +2,17 @@ package test.maths;
 
 public class I_ComputerMath {
     public static void main(String[] args) {
-        System.out.println(add(78, 25));
-        System.out.println(minus(78, 25));
-        System.out.println(multi(78, 25));
-        System.out.println(divide(78, 25));
+        System.out.println(add2(78, 25));
+        System.out.println(minus2(78, 25));
+        System.out.println(multi2(78, 25));
+        System.out.println(divide2(-1025, 25));
+        System.out.println(divide2(Integer.MIN_VALUE, 100));
+        System.out.println(divide2(123, Integer.MIN_VALUE));
     }
 
-    //a+b 等于a和b的异或 加上 a和b的与然后向左移1位
+    //a+b 等于a和b的异或 加上 a和b的与然后向左移1位,即 a和b的不进位和+a和b的进位，a和b的进位就是a&b往左移一位
     public static int add(int arg1,int arg2){
+        //因为不能出现+号，所以只有当arg2为0的时候才是答案。
         if (arg2==0){
             return arg1;
         }
@@ -47,7 +50,72 @@ public class I_ComputerMath {
         return result;
     }
 
-        public static int negative(int arg){
+    public static int negative(int arg){
         return ~arg+1;
+    }
+
+    public static int add2(int arg1,int arg2){
+        if (arg2==0){
+            return arg1;
+        }
+        return add2(arg1^arg2,(arg1&arg2)<<1);
+    }
+
+    public static int minus2(int arg1,int arg2){
+        return add2(arg1,negative2(arg2));
+    }
+
+    public static int negative2(int arg){
+        return ~arg+1;
+    }
+
+    public static int multi2(int arg1,int arg2){
+        int result = 0;
+        while (arg2!=0){
+            if ((arg2&1)==1){
+                result = result + arg1;
+            }
+            arg2=arg2>>1;
+            arg1=arg1<<1;
+        }
+        return result;
+    }
+
+    //由于int的最小值不能反转，因为int取值范围是-2^31 ~ 2^31-1，所以需要特殊判断
+    public static int divide2(int arg1,int arg2){
+        if (arg1==Integer.MIN_VALUE&&arg2==Integer.MIN_VALUE){
+            return 1;
+        }else if (arg2==Integer.MIN_VALUE){
+            return 0;
+        }else if (arg1==Integer.MIN_VALUE){
+            //arg1+1=a
+            //a/arg2=b
+            //arg1-(b*arg2)=c
+            //c/arg2=d
+            //b+d=e
+            if (arg2==1){
+                return arg1;
+            }
+            return add2(divideSuffix(add2(arg1,1),arg2),divideSuffix(minus2(arg1,multi2(divideSuffix(add2(arg1,1),arg2),arg2)),arg2));
+        }else {
+            return divideSuffix(arg1,arg2);
+        }
+    }
+
+    public static int divideSuffix(int arg1,int arg2){
+        int x = isNeg(arg1)?negative2(arg1):arg1;
+        int y = isNeg(arg2)?negative2(arg2):arg2;
+        int result = 0;
+        for (int i = 30; i >= 0; i= minus(i,1)) {
+            if ((x>>i)>=y){
+                result |=1<<i;
+                x=minus(x,y<<i);
+            }
+        }
+        return isNeg(arg1)^isNeg(arg2)?negative2(result):result;
+    }
+
+    public static boolean isNeg(int arg){
+        return arg<0;
     }
 }
